df9087cee8c66b4761b3ed360160dfe453be7f80
aroramrinaal <apply.mrinaal@gmail.com>
2024-07-20 13:11:42 -0700

    chore: Add Navbar component to stats page

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,9 +1,10 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 const StatsPage = () => {
     return (
-        <div>
-
+        <div className="bg-background">
+            <Navbar />
         </div>
     );
 };
