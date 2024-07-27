abbcbee9e7a1bcb8e6011a792614813544f1beda
aroramrinaal <apply.mrinaal@gmail.com>
2024-07-27 03:20:43 -0700

    feat: Improve responsive menu in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,29 +29,39 @@ const Navbar = () => {
           <span className={`absolute h-0.5 w-full bg-primary-foreground transform transition-all duration-300 ease-in-out ${isMenuOpen ? '-rotate-45' : 'translate-y-2'}`} />
         </div>
       </button>
-      <nav className={`${isMenuOpen ? 'block' : 'hidden'} w-full lg:block lg:w-auto bg-background text-primary-foreground lg:bg-transparent lg:text-foreground`} id="navbar-menu">
-        <ul className="flex flex-col lg:flex-row gap-4 lg:gap-6 lg:items-center">
-          <li>
-            <Link href="/" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="#" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
-              Webstore
-            </Link>
-          </li>
-          <li>
+      <nav className={`${isMenuOpen ? '' : 'hidden'} w-full lg:block lg:w-auto bg-background text-primary-foreground lg:bg-transparent lg:text-foreground`} id="navbar-menu">
+        <ul className="flex flex-col lg:flex-row gap-4 lg:gap-6 lg:items-center justify-center lg:justify-start py-8 lg:py-0">
+          <li className="lg:hidden">
             <Link href="/features" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
               Features
             </Link>
           </li>
-          <li>
+          <li className="lg:hidden">
+            <Link href="https://x.com/mrinaalarora25" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
+              Contact
+            </Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link href="/" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
+              Home
+            </Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link href="#" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
+              Webstore
+            </Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link href="/features" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
+              Features
+            </Link>
+          </li>
+          <li className="hidden lg:block">
             <Link href="https://github.com/aroramrinaal/cursorpath-landing" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
               Github
             </Link>
           </li>
-          <li>
+          <li className="hidden lg:block">
             <Link href="https://x.com/mrinaalarora25" className="text-xl font-medium hover:underline underline-offset-4" prefetch={false}>
               Contact
             </Link>
