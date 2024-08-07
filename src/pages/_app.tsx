5be5eb8b3e5e05d787d6c28e3fb0f35400eeb384
aroramrinaal <apply.mrinaal@gmail.com>
2024-08-07 11:57:50 -0700

    feat: Add Google site verification meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta name="google-site-verification" content="caST24rr9-_P8ReZVP96X6Sar71hHuzRFiJC0C3nC3U" />
         <title>Cursor Path | Track Your Mouse Journey</title>
       </Head>
       <Component {...pageProps} />
